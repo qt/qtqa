bae3525275d03a982c5d873db200a95abff983f3
Rohan McGovern <rohan.mcgovern@nokia.com>
2012-06-06 13:26:11 +1000

    Revert "Run qtqa tests in parallel."
    It is unfortunately _not_ generally safe to run these tests in parallel
    as previously thought.
    
    Known issues include at least the tests for the crash reporter plugin on
    Mac, but for now we'll revert this completely to ensure stable test
    results.
    
    This reverts commit 3da8f9e0f9e9492c1af5843aa61166347f9d6253.
    
    Change-Id: Ib047838aec07344e26a30fb3776c165f8735f7ab
    Reviewed-by: Toby Tomkins <toby.tomkins@nokia.com>

--- scripts/test.pl
+++ scripts/test.pl
@@ -190,9 +190,6 @@ sub run_prove
         #
         '--state=save',
 
-        # run in parallel (-j3 chosen fairly arbitrarily, should be safe)
-        '-j3',
-
         # Let tests freely use modules under lib/perl5
         '-I',
         "$FindBin::Bin/lib/perl5",
