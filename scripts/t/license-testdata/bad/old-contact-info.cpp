b2221faca328d173e4cb91b75a4cce90cb5568dd
Sergio Ahumada <sergio.ahumada@digia.com>
2013-01-11 19:02:51 +0100

    Update copyright year in Digia's license headers
    Change-Id: I02eaa709ff91d158a3e34f4e719254f2adce873e
    Reviewed-by: Simo Fält <simo.falt@digia.com>
    Reviewed-by: Janne Anttila <janne.anttila@digia.com>

--- scripts/t/license-testdata/bad/old-contact-info.cpp
+++ scripts/t/license-testdata/bad/old-contact-info.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************
 **
-** Copyright (C) 2012 Digia Plc and/or its subsidiary(-ies).
+** Copyright (C) 2013 Digia Plc and/or its subsidiary(-ies).
 ** Contact: Nokia Corporation (qt-info@nokia.com)
 **
 ** This file is part of the (whatever) of the Qt Toolkit.
