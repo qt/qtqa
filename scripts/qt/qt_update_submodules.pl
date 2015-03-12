6cf766415b2714c896379a4c61b8bebe01d651b0
Sergio Ahumada <sahumada@blackberry.com>
2015-03-12 20:03:18 +0100

    Make submodule update handle qt3d properly
    qt3d now also has a 5.5 branch in addition to its dev branch
    
    Change-Id: Ia546b32bab946cafdf2f23c585d72b75022b5e20
    Reviewed-by: Jani Heikkinen <jani.heikkinen@theqtcompany.com>
    Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@theqtcompany.com>

--- scripts/qt/qt_update_submodules.pl
+++ scripts/qt/qt_update_submodules.pl
@@ -81,7 +81,7 @@ sub get_submodule_ref
     my ($submodule, $qt_branch) = @_;
 
     my @submodules_based_only_on_master_branch = ('qtdocgallery', 'qtfeedback', 'qtjsondb', 'qtqa', 'qtrepotools');
-    my @submodules_based_only_on_dev_branch = ('qt3d','qtpim');
+    my @submodules_based_only_on_dev_branch = ('qtpim');
     my %submodules_based_on_custom_branch = (
          'qtenginio' =>
             {
