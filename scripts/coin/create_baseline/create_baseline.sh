cdfda1b40ac125bc5639582b0cea05d68e7b6993
Aapo Keskimolo <aapo.keskimolo@qt.io>
2017-09-06 14:39:34 +0300

    Add production baseline scripts
    Baseline creation and verification steps:
    1) Create production baseline
       - Repository qtqa/tqtc-coin-ci production branch HEAD is checked
         out followed by git merge origin/master
    2) Test production baseline
       - Run repository auto-tests with make citest
       - Schedule integration tests with run_builds.py cli
    
    If tests are successful, the user should amend the commit with test
    results and push the change to qtqa/tqtc-coin-ci repository (requires
    rights to push merges).
    
    NOTE: If any of the tests in step 2 fail, this should be mentioned
    in the commit message of the merge.
    
    Change-Id: I5f44f1a3ee9f52771b25e8ea6669978e90277d29
    Reviewed-by: Simo Fält <simo.falt@qt.io>

---
+++ scripts/coin/create_baseline/create_baseline.sh
@@ -0,0 +1,127 @@
+#!/bin/bash -e
+#############################################################################
+##
+## Copyright (C) 2017 The Qt Company Ltd.
+## Contact: https://www.qt.io/licensing/
+##
+## This file is part of the Quality Assurance module of the Qt Toolkit.
+##
+## $QT_BEGIN_LICENSE:GPL-EXCEPT$
+## Commercial License Usage
+## Licensees holding valid commercial Qt licenses may use this file in
+## accordance with the commercial license agreement provided with the
+## Software or, alternatively, in accordance with the terms contained in
+## a written agreement between you and The Qt Company. For licensing terms
+## and conditions see https://www.qt.io/terms-conditions. For further
+## information use the contact form at https://www.qt.io/contact-us.
+##
+## GNU General Public License Usage
+## Alternatively, this file may be used under the terms of the GNU
+## General Public License version 3 as published by the Free Software
+## Foundation with exceptions as appearing in the file LICENSE.GPL3-EXCEPT
+## included in the packaging of this file. Please review the following
+## information to ensure the GNU General Public License requirements will
+## be met: https://www.gnu.org/licenses/gpl-3.0.html.
+##
+## $QT_END_LICENSE$
+##
+#############################################################################
+
+# This script will clone coin repository and build its binaries.
+#
+# Usage:
+# ./create_baseline
+# ./create_baseline {commit-id}
+
+########### Variables #############
+
+. utils
+basepath=$(dirname $(readlink -f create_baseline.sh))  # absolute script path
+commit_template_file=$basepath/commit-msg-template
+test_script=$basepath/test_baseline.sh
+workdir=$repodir
+
+########### Functions #############
+
+remove_dir() {
+ rm -rf $1
+}
+
+clean_install() {
+ echo "Git reset production branch to HEAD to discard untracked changes / local commits."
+ # reset source/git files and clean all caches and untracked files
+ git reset --hard origin/production && git clean -xdf -f
+}
+
+clone_tqtc_coin_ci() {
+ git clone -b production ssh://codereview.qt-project.org:29418/qtqa/tqtc-coin-ci $1
+}
+
+########### Main #############
+
+is_user_vmbuilder
+
+if [ ! -z "$1" ]; then
+ master_commit_id=$1
+ echo "Merging master commit-id:" $master_commit_id "into production"
+fi
+
+# if local coin repo does not exist, clone it from git
+if [[ ! -d $repodir ]]; then
+ echo "Local coin repository $repodir does not exist. Cloning from git.."
+ mkdir -p $repodir
+ clone_tqtc_coin_ci $repodir
+fi
+
+cd $repodir
+echo "Changed working directory:" $(pwd)
+
+# save current branch state that can be restored later
+git branch -f old_state
+# checkout current production head and update git refs
+git checkout production && git fetch
+
+ask_user_to_exec "Do you want to discard old cache/untracked files/binaries and remake the project from scratch? " "clean_install"
+
+# merge master into production
+commit_old=$(git rev-parse HEAD)
+if [ ! -z "$master_commit_id" ]; then
+ if git branch -r --contains $master_commit_id | grep -q 'master'; then
+  echo "Merging commit" $master_commit_id $suffix_text "from origin/master into production..."
+  git merge "$master_commit_id" -m "$(cat $commit_template_file)" --no-edit
+ else
+  echo "Remote-tracking (master) branch has no commit: $master_commit_id !"
+  exit 2
+ fi
+else
+ master_commit_id=$(git rev-parse origin/master)
+ suffix_text="(HEAD)"
+ git merge origin/master -m "$(cat $commit_template_file)" --no-edit
+fi
+
+if [ ! -f env/bin/activate ]; then
+ # creating coin binaries and webui
+ make -j1
+fi
+
+merge_tip_commit=$(git log --no-merges -1)
+merge_tip_commit_short=$(git log --no-merges -1 --oneline)
+echo -e "\nCommits added on top of the previous successful merge:\n"
+git log origin/production..HEAD --no-merges --decorate --oneline
+echo -e "\n******** PRODUCTION MERGE TIP COMMIT *******\n$(git log --no-merges -1)\n***********************************\n"
+
+# display commits that were added on top of the current production
+if [[ ! -z "$skip" ]]; then
+ gitdir=$(git rev-parse --git-dir)
+ scp -p -P 29418 codereview.qt-project.org:hooks/commit-msg .git/hooks/
+ git submodule update --init --checkout secrets
+ echo -e "\nProduction baseline has been created in $workdir/$repo"
+ git log -1 --merges
+else
+ skip=
+ git reset --hard old_state -q
+ git branch -D old_state
+ exit 2
+fi
+
+echo "To continue testing the baseline, run script" $test_script
---
+++ scripts/coin/create_baseline/test_baseline.sh
@@ -0,0 +1,78 @@
+#!/bin/bash -e
+#############################################################################
+##
+## Copyright (C) 2017 The Qt Company Ltd.
+## Contact: https://www.qt.io/licensing/
+##
+## This file is part of the Quality Assurance module of the Qt Toolkit.
+##
+## $QT_BEGIN_LICENSE:GPL-EXCEPT$
+## Commercial License Usage
+## Licensees holding valid commercial Qt licenses may use this file in
+## accordance with the commercial license agreement provided with the
+## Software or, alternatively, in accordance with the terms contained in
+## a written agreement between you and The Qt Company. For licensing terms
+## and conditions see https://www.qt.io/terms-conditions. For further
+## information use the contact form at https://www.qt.io/contact-us.
+##
+## GNU General Public License Usage
+## Alternatively, this file may be used under the terms of the GNU
+## General Public License version 3 as published by the Free Software
+## Foundation with exceptions as appearing in the file LICENSE.GPL3-EXCEPT
+## included in the packaging of this file. Please review the following
+## information to ensure the GNU General Public License requirements will
+## be met: https://www.gnu.org/licenses/gpl-3.0.html.
+##
+## $QT_END_LICENSE$
+##
+#############################################################################
+
+# This script will prepare the coin repository for testing.
+# NOTE: Any active Coin sessions will be terminated
+
+########### Variables #############
+
+. utils
+basepath=$(dirname $(readlink -f test_baseline.sh))  # absolute script path
+localmode=
+
+########### Functions #############
+
+function exec_builds() {
+ ./run_ci -r --tmux-no-attach
+ sleep 2
+ echo "Scheduled builds:"
+ /bin/bash -x $basepath/schedules/run_builds
+}
+
+########### Main #############
+
+if [ "$1" == "local" ]; then
+ localmode=1
+else
+ # if the script is run on the production server, verify user/network information
+ is_user_vmbuilder
+ check_network_interface
+fi
+
+cd $repodir
+. env/bin/activate
+
+ask_user_to_exec "Run make citests? " "make citest"
+
+# schedule integrations
+
+ask_user_to_exec "This will terminate any active Coin sessions. Do you want to continue? " "exec_builds"
+if [ ! -z $skip ]; then
+ if [ ! -z $localmode ]; then
+  webserver_ip=localhost
+ else
+  webserver_ip=$vmbuilder_ip
+ fi
+ if [ -z $QTCI_WEBSERVER_PORT ]; then
+  webserver_port=8080
+ else
+  webserver_port=$QTCI_WEBSERVER_PORT
+ fi
+ echo "To see Coin status on your browser, open link:" http://$webserver_ip:$webserver_port/coin
+fi
