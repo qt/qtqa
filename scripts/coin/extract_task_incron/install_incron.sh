4132995e596add89465950d33aa95ce7f6e25e83
Joni Jantti <joni.jantti@qt.io>
2018-07-26 09:26:26 +0300

    Use yum instead of apt for the build artifact extraction enabler script
    The server is running CentOs so we need to use yum for package
    management instead of apt.
    
    Change-Id: I5c17a3ec6f16805b2f0d8bc668d12d67bdd96eb9
    Reviewed-by: Aapo Keskimolo <aapo.keskimolo@qt.io>

--- scripts/coin/extract_task_incron/install_incron.sh
+++ scripts/coin/extract_task_incron/install_incron.sh
@@ -2,6 +2,6 @@
 
 # Run this script on the file server as root to enable automatic extraction of replicated coin build artifact packages.
 
-sudo apt-get -y install incron
+sudo yum -y install incron
 echo "QT" | sudo tee -a /etc/incron.allow
 sudo incrontab -u QT incrontab.txt
