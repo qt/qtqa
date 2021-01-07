bae800dc18a471bd2704d40ec82af54b856887c4
Jani Heikkinen <jani.heikkinen@qt.io>
2021-01-07 15:56:07 +0200

    Fix license testing in tqtc repositories
    Change-Id: I4e755ad40ec1c102afccae52492cd004a96c7209
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>
    Reviewed-by: Toni Saario <toni.saario@qt.io>
    (cherry picked from commit 3d714c2e2db4bf061aacccd55e7197863039304c)

--- tests/prebuild/license/tst_licenses.pl
+++ tests/prebuild/license/tst_licenses.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/env perl
 #############################################################################
 ##
-## Copyright (C) 2017 The Qt Company Ltd.
+## Copyright (C) 2021 The Qt Company Ltd.
 ## Contact: https://www.qt.io/licensing/
 ##
 ## This file is part of the test suite of the Qt Toolkit.
@@ -606,6 +606,9 @@ sub run
 
     # Get module name without the preceding path
     $moduleName = defined($optModuleName) ? $optModuleName : basename($QT_MODULE_TO_TEST);
+
+    # Remove possible 'tqtc-' prefix from the module name
+    substr($moduleName, 0, 5, "") if (index($moduleName,"tqtc-") == 0);
 
     # Skip the test (and return success) if we don't want to scan this module
 
