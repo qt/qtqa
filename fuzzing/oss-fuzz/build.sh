c9a3810097985ba977a09adc21a093a1a4cf97a7
Robert Löhning <robert.loehning@qt.io>
2021-03-26 08:38:53 +0100

    oss-fuzz: Add fuzz target for QDateTime
    Change-Id: I655a74808714629af693deec30d4286c2ca205d1
    Reviewed-by: Edward Welbourne <edward.welbourne@qt.io>

--- fuzzing/oss-fuzz/build.sh
+++ fuzzing/oss-fuzz/build.sh
@@ -34,6 +34,7 @@ VERBOSE=1 cmake --build . --parallel
 
 # prepare corpus files
 zip -j $WORK/cbor $SRC/qtqa/fuzzing/testcases/cbor/*
+zip -j $WORK/datetime $SRC/qtqa/fuzzing/testcases/datetime/*
 zip -j $WORK/html $SRC/qtqa/fuzzing/testcases/html/*
 zip -j $WORK/icc $SRC/qtqa/fuzzing/testcases/icc/*
 zip -j $WORK/images $SRC/qtqa/fuzzing/testcases/{bmp,gif,ico,jpg,png,svg,xbm,xpm}/* $SRC/afltestcases/images/*/*
@@ -82,6 +83,7 @@ build_fuzzer "qtbase" "corelib/serialization/qcborvalue/fromcbor/fromcbor.pro" "
 build_fuzzer "qtbase" "corelib/serialization/qtextstream/extractionoperator-float/extractionoperator-float.pro" "text"
 build_fuzzer "qtbase" "corelib/serialization/qxmlstream/qxmlstreamreader/readnext/readnext.pro" "xml" "$SRC/afldictionaries/xml.dict"
 build_fuzzer "qtbase" "corelib/text/qregularexpression/optimize/optimize.pro" "regexp" "$SRC/afldictionaries/regexp.dict"
+build_fuzzer "qtbase" "corelib/time/qdatetime/fromstring/fromstring.pro" "datetime"
 build_fuzzer "qtbase" "corelib/tools/qcryptographichash/result/result.pro"
 build_fuzzer "qtbase" "gui/image/qimage/loadfromdata/loadfromdata.pro" "images"
 build_fuzzer "qtbase" "gui/painting/qcolorspace/fromiccprofile/fromiccprofile.pro" "icc" "$SRC/afldictionaries/iccprofile.dict"
