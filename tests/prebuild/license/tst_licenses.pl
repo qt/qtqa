cca09738062e4ab6c34deeee5c6edb4f069cd750
Friedemann Kleint <Friedemann.Kleint@qt.io>
2021-11-11 13:24:18 +0100

    tst_licenses.pl: Prefix each fail by "error:"
    tst_licenses.pl uses the Test::More testing module, which reports
    errors as "not ok". As this is not easily discoverable in a CI log,
    prefix it by "error:".
    
    Change-Id: I80daa2c12a877d582d7f48ffb51ff7f861f2d8aa
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>
    (cherry picked from commit 35d8e7c7891f096091052dcaeecc15e26c73f172)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- tests/prebuild/license/tst_licenses.pl
+++ tests/prebuild/license/tst_licenses.pl
@@ -292,7 +292,7 @@ sub loadLicense {
     # Read the sample license
     my $fileHandle;
     if (!open($fileHandle, '<', $licenseFile)) {
-        fail("Cannot open license file: $licenseFile");
+        fail("error: Cannot open license file: $licenseFile");
         return 0;
     }
 
@@ -309,7 +309,7 @@ sub loadLicense {
         }
     }
     if (!$foundBeginMarker) {
-        fail("$licenseFile has no QT_BEGIN_LICENSE marker");
+        fail("error: $licenseFile has no QT_BEGIN_LICENSE marker");
         close $fileHandle;
         return 0;
     }
@@ -330,7 +330,7 @@ sub loadLicense {
     }
     close $fileHandle;
     if (!$foundEndMarker) {
-        fail("$licenseFile has no QT_END_LICENSE marker");
+        fail("error: $licenseFile has no QT_END_LICENSE marker");
         return 0;
     }
 
@@ -346,7 +346,7 @@ sub loadLicense {
 sub msgMismatch
 {
     my ($filename, $actual, $reference, $licenseType, $line) = @_;
-    return "Mismatch in license text in\n" . $filename . "\n"
+    return "error: Mismatch in license text in\n" . $filename . "\n"
         . "    Actual: '" . $actual . "'\n"
         . "  Expected: '" . $reference . "'\n"
         . '   License: ' . $licenseType . ' (' . $licenseFiles{$licenseType} . ':'
@@ -368,7 +368,7 @@ sub checkLicense
     # Read in the whole file
     my $fileHandle;
     if (!open($fileHandle, '<', $filename)) {
-        fail("Cannot open $filename");
+        fail("error: Cannot open $filename");
         return 0;
     }
     my @lines = <$fileHandle>;
@@ -431,15 +431,15 @@ sub checkLicense
                 ($licenseType,$endDelimiter) = ($1, $2);
                 # Verify that we have reference text for the license type
                 if (!@{$licenseTexts{$licenseType} // []}) {
-                    fail("No reference text for license type $licenseType in $shortfilename, line $currentLine");
+                    fail("error: No reference text for license type $licenseType in $shortfilename, line $currentLine");
                     return 0;
                 }
                 $inLicenseText = 1;
             } elsif (/^\Q$beginDelimiter\E/) {
-                fail("QT_BEGIN_LICENSE does not follow Copyright block in $shortfilename, line $currentLine");
+                fail("error: QT_BEGIN_LICENSE does not follow Copyright block in $shortfilename, line $currentLine");
                 return 0;
             } else {
-                fail("$shortfilename has license header with inconsistent comment delimiters, line $currentLine");
+                fail("error: $shortfilename has license header with inconsistent comment delimiters, line $currentLine");
                 return 0;
             }
             $linesMatched++;
@@ -457,7 +457,7 @@ sub checkLicense
                 }
                 my $useOldText = 0;
                 if ($#text != $#referenceText) {
-                    my $message = 'License text (' . $#text . ') and reference text ('
+                    my $message = 'error: License text (' . $#text . ') and reference text ('
                         . $licenseType . ', ' . $#referenceText . ') have different number of lines in '
                         . $shortfilename;
                     if ($#oldReferenceText == 0) {
@@ -477,7 +477,7 @@ sub checkLicense
                 while ($n <= $#text) {
                     if ($text[$n] ne $referenceText[$n]) {
                         if (!$useOldText && $hasOldText) {
-                            print('License text does not match ' . $licenseType . ' due to: '
+                            print('error: License text does not match ' . $licenseType . ' due to: '
                                   . msgMismatch($shortfilename, $text[$n], $referenceText[$n],
                                                 $licenseType, $n) . "\n");
                             $useOldText = 1;
@@ -521,7 +521,7 @@ sub checkLicense
 
     # Did we find any valid licenses?
     if ($matchedLicenses == 0 && $#lines > 2) {
-        fail("$shortfilename does not appear to contain a license header");
+        fail("error: $shortfilename does not appear to contain a license header");
         return 0;
     }
 
