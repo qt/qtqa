cf53bf5caf66a457e5385a7cc77f4a270f7ec155
Audun Sutterud <audun.sutterud@qt.io>
2023-11-03 15:32:31 +0100

    corebenchrunner: Remove a test dependency
    The test depends on the default configuration file. This causes issues
    when modifying the configuration file for testing purposes.
    
    Change-Id: I0017b810f1d735b86934714a8d946729557988ff
    Reviewed-by: Daniel Smith <daniel.smith@qt.io>

--- scripts/corebenchrunner/tests/test_runner.py
+++ scripts/corebenchrunner/tests/test_runner.py
@@ -1,6 +1,7 @@
 # Copyright (C) 2023 The Qt Company Ltd.
 # SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
-import os
+import json
+import tempfile
 import unittest
 from typing import cast
 
@@ -20,11 +21,19 @@ class TestArguments(unittest.TestCase):
 class TestConfiguration(unittest.TestCase):
     def test_load(self) -> None:
         """
-        We can detect errors in the default configuration file.
+        We can detect errors in a configuration file.
         """
-        directory = os.path.dirname(os.path.dirname(__file__))
-        file = os.path.join(directory, "config.json")
-        configuration = runner.Configuration.load(file=file, skip_upload=False)
+        with tempfile.NamedTemporaryFile(mode="w") as f:
+            json.dump(
+                {
+                    "coordinator_info": {"url": "https://coordinator.com/", "secret": ""},
+                    "qtbase_git_remote": {"url": "ssh://codereview.qt-project.org/qt/qtbase"},
+                },
+                f,
+            )
+            f.seek(0)
+            configuration = runner.Configuration.load(file=f.name, skip_upload=True)
+
         self.assertIsInstance(configuration, runner.Error)
         error = cast(runner.Error, configuration)
         self.assertEqual(error.message.splitlines()[0], "Configuration file contains errors:")
