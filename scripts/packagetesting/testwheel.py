704705aac9e02424276cddf7d7cf4fe963155eef
Friedemann Kleint <Friedemann.Kleint@qt.io>
2026-01-29 14:01:25 +0100

    testwheel.py: Update to 6.10.2
    - Adapt to renaming of the player example
    - Add svgtoqml depending on version
    - Fix flake8 warnings
    
    Change-Id: Id00f2caa21ac5c12fd1ce1ccbae87b3b2189dcc7
    Reviewed-by: Shyamnath Premnadh <Shyamnath.Premnadh@qt.io>
    (cherry picked from commit 00d0343e20fb70c9d377ec4873471df81a9a104f)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 45cfa54fa8b34db09f5ce430e32193e708f5c3b3)

--- scripts/packagetesting/testwheel.py
+++ scripts/packagetesting/testwheel.py
@@ -28,9 +28,6 @@ PYINSTALLER_EXAMPLE_2 = 'widgets/widgets/tetrix.py'
 OPCUAVIEWER = 'opcua/opcuaviewer/main.py'
 WEBENGINE_TABBED_BROWSER = 'webenginewidgets/tabbedbrowser/main.py'
 PROJECT_TOOL = "pyside6-project"
-ESSENTIAL_TOOLS = ["deploy", "genpyi", ("lrelease", "-help"), "lupdate", "metaobjectdump",
-         "project", "qml", "qmlformat", ("qmlimportscanner", "-importPath", "."), "qmllint",
-         "qmlls", "qmltyperegistrar", "qtpy2cpp", "rcc", "uic"]
 ADDONS_TOOLS = ["qsb", "balsam", "balsamui"]
 
 
@@ -95,8 +92,8 @@ def get_installed_modules():
     """Return installed modules"""
     result = []
     _, lines = run_process([sys.executable, "-m", "pip", "list"])
-    for l in lines:
-        tokens = l.split(' ')
+    for line in lines:
+        tokens = line.split(' ')
         if len(tokens) >= 1:
             result.append(tokens[0].lower())
     return result
@@ -107,11 +104,20 @@ def has_module(name):
     return name.lower() in get_installed_modules()
 
 
+def get_essential_tools():
+    result = ["deploy", "genpyi", ("lrelease", "-help"), "lupdate", "metaobjectdump",
+              "project", "qml", "qmlformat", ("qmlimportscanner", "-importPath", "."), "qmllint",
+              "qmlls", "qmltyperegistrar", "qtpy2cpp", "rcc", "uic"]
+    if VERSION >= (6, 10, 0):
+        result.append("svgtoqml")
+    return result
+
+
 def get_installed_tools():
     """Find the installed tools based on the wheels installed"""
     if (has_module("PySide6_Addons") or has_module("PySide6-Addons")) and VERSION >= (6, 6, 0):
-        return ESSENTIAL_TOOLS + ADDONS_TOOLS
-    return ESSENTIAL_TOOLS
+        return get_essential_tools() + ADDONS_TOOLS
+    return get_essential_tools()
 
 
 def get_installed_wheels(examples_root):
@@ -160,8 +166,9 @@ def get_addon_examples():
     else:
         result.append('datavisualization/bars3d/bars3d.py')
         result.append(WEBENGINE_TABBED_BROWSER)
+    player = "main.py" if VERSION >= (6, 10, 2) else "player.py"
     result.extend(['3d/simple3d/simple3d.py', 'charts/chartthemes/main.py',
-                   'multimedia/player/player.py'])
+                   f'multimedia/player/{player}'])
     return result
 
 
@@ -263,7 +270,7 @@ def test_deploy(example):
 
 
 def test_cxfreeze(example):
-    assert(example.is_file())
+    assert (example.is_file())
     print(f'Running CxFreeze test of {example.stem}')
     current_dir = os.getcwd()
     result = False
@@ -285,7 +292,7 @@ def test_cxfreeze(example):
 
 
 def test_pyinstaller(example):
-    assert(example.is_file())
+    assert (example.is_file())
     print(f'Running PyInstaller test of {example.stem}')
     current_dir = os.getcwd()
     result = False
